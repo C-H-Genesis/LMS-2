183b6b4e3502439b1b16ba802f3b2bdf3bfdec5c
C-H-Genesis <chrishaston2@gmail.com>
2025-10-20 17:44:47 +0200

    Update backend URL

--- dist/sms-lms/server/angular-app-manifest.mjs
+++ dist/sms-lms/server/angular-app-manifest.mjs
@@ -14,8 +14,8 @@ export default {
   ]
 },
   assets: {
-    'index.csr.html': {size: 11024, hash: '8108fac2efdd3f69c50d73f8ada554e3831bdb51672198054f396fd5af188cf6', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1318, hash: '21993a938436aa028e2ddf47fe72eedb407c4cde5895cefa539ac12f2afd3c0e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 11024, hash: '2689f83c04653d372f1a8dbe014123eaa527d44ac93d2d4fc8c1ed204a847e44', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1318, hash: '0e2d1a2a1460448c78afd7201fff68113f4f902dbdb348e15480fb6ff148990d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-QNNFEVIC.css': {size: 568261, hash: 'lPBOPPe+qD4', text: () => import('./assets-chunks/styles-QNNFEVIC_css.mjs').then(m => m.default)}
   },
 };
